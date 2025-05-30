77853ea0a8650a2cb6762e75d08abdf4cc8991bf
Mitto <mitto.disotta@proton.me>
2025-05-29 23:27:51 -0400

    Commit

---
+++ lib/apiClient.ts
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'https://api-service.vercel.app/api',
+  headers: {
+    'Content-Type': 'application/json'
+  },
+  timeout: 5000
+});
+
+export default apiClient;
